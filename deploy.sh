2049e5f29256604cb3b0a6ad036520eb0d258bfd
strong <strong@boodskapper.com>
2023-12-11 03:36:13 +0700

    update dockerfile

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ sudo docker stop running-vnexpress-crawler
 sudo docker rm running-vnexpress-crawler
 sudo docker rmi vnexpress-crawler
 sudo docker build -t vnexpress-crawler -f Dockerfile.VNExpressCrawler .
-# sudo docker run -d --name running-vnexpress-crawler vnexpress-crawler
+sudo docker run -d --name running-vnexpress-crawler -v $(pwd)/data:/app/data vnexpress-crawler
 
 sudo docker stop running-tuoitre-crawler
 sudo docker rm running-tuoitre-crawler
 sudo docker rmi tuoitre-crawler
 sudo docker build -t tuoitre-crawler -f Dockerfile.TuoiTreCrawler .
-# sudo docker run -d --name running-vnexpress-crawler vnexpress-crawler
+sudo docker run -d --name running-tuoitre-crawler -v $(pwd)/data:/app/data tuoitre-crawler
 
-# sudo docker logs -f running-vnexpress-crawler
+# sudo docker logs -f running-tuoitre-crawler
